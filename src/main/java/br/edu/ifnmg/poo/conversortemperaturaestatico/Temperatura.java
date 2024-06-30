d45a6981fa91199b36f8628601768f567a2f05b0
AnaLuizanc <analuizanc13@gmail.com>
2024-06-30 12:06:25 -0300

    add getters e setters

--- src/main/java/br/edu/ifnmg/poo/conversortemperaturaestatico/Temperatura.java
+++ src/main/java/br/edu/ifnmg/poo/conversortemperaturaestatico/Temperatura.java
@@ -5,9 +5,17 @@ package br.edu.ifnmg.poo.conversortemperaturaestatico;
  * @author analu
  */
 public class Temperatura {
+
     static double temperatura;
 
-    
-    
-    
+    //<editor-fold defaultstate="collapsed" desc="Getters / Setters">
+    public static double getTemperatura() {
+        return temperatura;
+    }
+
+    public static void setTemperatura(double temperatura) {
+        Temperatura.temperatura = temperatura;
+    }
+//</editor-fold>
+
 }
